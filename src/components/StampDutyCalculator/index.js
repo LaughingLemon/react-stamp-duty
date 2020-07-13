514179e5d82ef57136da5d5c19378ef3e5d243e4
LaughingLemon <laughing.programmer@outlook.com>
2020-07-13 23:42:17 +0100

    Add amount prop to StampDutyCalculator

--- src/components/StampDutyCalculator/index.js
+++ src/components/StampDutyCalculator/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './style.css';
 import CalculatorLine from "../CalculatorLine";
 
-export default () => (
-    <table className="StampDutyCalculator">
-        <tr><th>Band</th><th>Amount Taxable</th><th>Stamp Duty</th></tr>
-        <CalculatorLine lowerBound={125001} upperBound={250000} rate={0.02} />
-        <CalculatorLine lowerBound={250001} upperBound={925000} rate={0.05} />
-        <CalculatorLine lowerBound={925001} upperBound={1500000} rate={0.10} />
-        <CalculatorLine lowerBound={1500001} rate={0.12} />
-    </table>
+export default (props) => (
+    <div className="StampDutyCalculator">
+        <div><label for="amount">Amount</label><input type="number" name="amount" id="amount" /></div>
+        <table>
+            <tr><th>Band</th><th>Amount Taxable</th><th>Stamp Duty</th></tr>
+            <CalculatorLine lowerBound={125001} upperBound={250000} amount={props.amount} rate={0.02} />
+            <CalculatorLine lowerBound={250001} upperBound={925000} amount={props.amount} rate={0.05} />
+            <CalculatorLine lowerBound={925001} upperBound={1500000} amount={props.amount} rate={0.10} />
+            <CalculatorLine lowerBound={1500001} amount={props.amount} rate={0.12} />
+        </table>
+    </div>
 );
--- src/components/StampDutyCalculator/index.test.js
+++ src/components/StampDutyCalculator/index.test.js
@@ -8,11 +8,31 @@ test('renders StampDutyCalculator container', () => {
     renderer.render(<StampDutyCalculator />);
     const result = renderer.getRenderOutput();
 
-    expect(result.type).toBe('table');
+    expect(result.type).toBe('div');
     expect(result.props.children).toEqual([
-        <tr><th>Band</th><th>Amount Taxable</th><th>Stamp Duty</th></tr>,
-        <CalculatorLine lowerBound={125001} upperBound={250000} rate={0.02} />,
-        <CalculatorLine lowerBound={250001} upperBound={925000} rate={0.05} />,
-        <CalculatorLine lowerBound={925001} upperBound={1500000} rate={0.10} />,
-        <CalculatorLine lowerBound={1500001} rate={0.12} />]);
+        <div><label for="amount">Amount</label><input type="number" name="amount" id="amount" /></div>,
+        <table>
+        <tr><th>Band</th><th>Amount Taxable</th><th>Stamp Duty</th></tr>
+        <CalculatorLine lowerBound={125001} upperBound={250000} rate={0.02} />
+        <CalculatorLine lowerBound={250001} upperBound={925000} rate={0.05} />
+        <CalculatorLine lowerBound={925001} upperBound={1500000} rate={0.10} />
+        <CalculatorLine lowerBound={1500001} rate={0.12} />
+        </table>]);
+});
+
+test('renders with amount', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<StampDutyCalculator amount={750000}/>);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual([
+        <div><label for="amount">Amount</label><input type="number" name="amount" id="amount" /></div>,
+        <table>
+        <tr><th>Band</th><th>Amount Taxable</th><th>Stamp Duty</th></tr>
+        <CalculatorLine lowerBound={125001} upperBound={250000} amount={750000} rate={0.02} />
+        <CalculatorLine lowerBound={250001} upperBound={925000} amount={750000} rate={0.05} />
+        <CalculatorLine lowerBound={925001} upperBound={1500000} amount={750000} rate={0.10} />
+        <CalculatorLine lowerBound={1500001} amount={750000} rate={0.12} />
+        </table>]);
 });
