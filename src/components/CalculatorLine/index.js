2cb3fecfbf4ead597155f2dd3746bab3feb7a92f
LaughingLemon <laughing.programmer@outlook.com>
2020-07-13 23:41:36 +0100

    Add bound event to CalculatorLine.

--- src/components/CalculatorLine/index.js
+++ src/components/CalculatorLine/index.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import './style.css';
 
-const CalculatorLine = (props) => {
-    const formatter = new Intl.NumberFormat('en-UK', {
-        style: 'currency',
-        currency: 'GBP',
-        minimumFractionDigits: 2
-    });
-
-    const lower = formatter.format(props.lowerBound);
-    const upper = props.lowerBound > 0.00 && props.upperBound === 0.00 ? "Above" : formatter.format(props.upperBound);
-    let taxable = 0.00;
-    if (props.amount > 0.00 && props.amount >= props.upperBound) {
-        taxable = props.upperBound - props.lowerBound;
-    } else
-    if (props.amount > 0.00 && props.amount >= props.lowerBound) {
-        taxable = props.amount - props.lowerBound;
+class CalculatorLine extends React.Component {
+    constructor(props) {
+        super(props);
+        this.changeTax = this.changeTax.bind(this);
     }
 
-    const tax = taxable * props.rate;
+    changeTax(tax) {
+        this.props.onTaxChange && this.props.onTaxChange(tax);  
+    }
+    
+    render() {
+        const formatter = new Intl.NumberFormat('en-UK', {
+            style: 'currency',
+            currency: 'GBP',
+            minimumFractionDigits: 2
+        });
+        
+        const lower = formatter.format(this.props.lowerBound);
+        const upper = this.props.lowerBound > 0.00 && 
+                      this.props.upperBound === 0.00 ? 
+                      "Above" : 
+                      formatter.format(this.props.upperBound);
 
-    return (
-        <tr className="CalculatorLine">
-            <td>Band {lower} to {upper} at {(props.rate * 100) + "%"}</td>
-            <td>{formatter.format(taxable)}</td><td>{formatter.format(tax)}</td>
-        </tr>
-    );
-};
+        let taxable = 0.00;
+        if (this.props.amount > 0.00 && this.props.amount >= this.props.upperBound) {
+            taxable = this.props.upperBound - this.props.lowerBound;
+        } else
+        if (this.props.amount > 0.00 && this.props.amount >= this.props.lowerBound) {
+            taxable = this.props.amount - this.props.lowerBound;
+        }
+
+        const tax = taxable * this.props.rate;
+        
+        this.changeTax(+tax.toFixed(2));
+
+        return (
+            <tr className="CalculatorLine">
+                <td>Band {lower} to {upper} at {(this.props.rate * 100) + "%"}</td>
+                <td>{formatter.format(taxable)}</td><td>{formatter.format(tax)}</td>
+            </tr>
+        );       
+    }
+}
 
 CalculatorLine.defaultProps = {
     lowerBound: 0.00, 
--- src/components/CalculatorLine/index.test.js
+++ src/components/CalculatorLine/index.test.js
@@ -3,64 +3,79 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import CalculatorLine from './';
 
 test('renders CalculatorLine container', () => {
+    const onTaxChangeMock = jest.fn();
+    
     const renderer = new ShallowRenderer();
-    renderer.render(<CalculatorLine />);
+    renderer.render(<CalculatorLine onTaxChange={onTaxChangeMock} />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe('tr');
     expect(result.props.children).toEqual([<td>Band {'£0.00'} to {'£0.00'} at {'0%'}</td>,<td>{'£0.00'}</td>,<td>{'£0.00'}</td>]);
+    expect(onTaxChangeMock).toBeCalledWith(0.00);
 });
 
 test('CalculatorLine with upper bound set', () => {
+    const onTaxChangeMock = jest.fn();
     const renderer = new ShallowRenderer();
-    renderer.render(<CalculatorLine upperBound={50000} />);
+    renderer.render(<CalculatorLine upperBound={50000} onTaxChange={onTaxChangeMock} />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe('tr');
     expect(result.props.children).toEqual([<td>Band {'£0.00'} to {'£50,000.00'} at {'0%'}</td>,<td>{'£0.00'}</td>,<td>{'£0.00'}</td>]);
+    expect(onTaxChangeMock).toBeCalledWith(0.00);
 });
 
 test('CalculatorLine with lower bound set', () => {
+    const onTaxChangeMock = jest.fn();
     const renderer = new ShallowRenderer();
-    renderer.render(<CalculatorLine lowerBound={50000} />);
+    renderer.render(<CalculatorLine lowerBound={50000}  onTaxChange={onTaxChangeMock} />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe('tr');
     expect(result.props.children).toEqual([<td>Band {'£50,000.00'} to {'Above'} at {'0%'}</td>,<td>{'£0.00'}</td>,<td>{'£0.00'}</td>]);
+    expect(onTaxChangeMock).toBeCalledWith(0.00);
 });
 
 test('CalculatorLine with amount set', () => {
+    const onTaxChangeMock = jest.fn();
     const renderer = new ShallowRenderer();
-    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={55000} />);
+    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={55000} onTaxChange={onTaxChangeMock} />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe('tr');
     expect(result.props.children).toEqual([<td>Band {'£50,000.00'} to {'£59,999.99'} at {'0%'}</td>,<td>{'£5,000.00'}</td>,<td>{'£0.00'}</td>]);
+    expect(onTaxChangeMock).toBeCalledWith(0.00);
 });
 
 test('CalculatorLine with rate set', () => {
+    const onTaxChangeMock = jest.fn();
     const renderer = new ShallowRenderer();
-    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={55000} rate={0.05} />);
+    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={55000} rate={0.05} onTaxChange={onTaxChangeMock} />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe('tr');
     expect(result.props.children).toEqual([<td>Band {'£50,000.00'} to {'£59,999.99'} at {'5%'}</td>,<td>{'£5,000.00'}</td>,<td>{'£250.00'}</td>]);
+    expect(onTaxChangeMock).toBeCalledWith(250.00);
 });
 
 test('CalculatorLine above band', () => {
+    const onTaxChangeMock = jest.fn();
     const renderer = new ShallowRenderer();
-    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={65000} rate={0.05} />);
+    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={65000} rate={0.05}  onTaxChange={onTaxChangeMock} />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe('tr');
     expect(result.props.children).toEqual([<td>Band {'£50,000.00'} to {'£59,999.99'} at {'5%'}</td>,<td>{'£9,999.99'}</td>,<td>{'£500.00'}</td>]);
+    expect(onTaxChangeMock).toBeCalledWith(500.00);
 });
 
 test('CalculatorLine below band', () => {
+    const onTaxChangeMock = jest.fn();
     const renderer = new ShallowRenderer();
-    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={45000} rate={0.05} />);
+    renderer.render(<CalculatorLine lowerBound={50000.00} upperBound={59999.99} amount={45000} rate={0.05}  onTaxChange={onTaxChangeMock} />);
     const result = renderer.getRenderOutput();
 
     expect(result.type).toBe('tr');
     expect(result.props.children).toEqual([<td>Band {'£50,000.00'} to {'£59,999.99'} at {'5%'}</td>,<td>{'£0.00'}</td>,<td>{'£0.00'}</td>]);
+    expect(onTaxChangeMock).toBeCalledWith(0.00);
 });
